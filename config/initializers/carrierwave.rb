e502b4ac6627dbc198aeaaa4c5c7646d10ae4402
EC2 Default User <ec2-user@ip-172-31-9-92.ap-northeast-1.compute.internal>
2020-10-27 17:48:06 +0900

    blog-deploy-after-s3-version2

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,16 +1,15 @@
+require 'carrierwave/storage/abstract'
+require 'carrierwave/storage/file'
 require 'carrierwave/storage/fog'
 
+if Rails.env.production?
   CarrierWave.configure do |config|
-    if Rails.env.production?
-    config.fog_provider = 'fog/aws'
     config.fog_credentials = {
-      provider:              'AWS',
-      aws_access_key_id:     Rails.application.credentials.aws[:access_key_id],
-      aws_secret_access_key: Rails.application.credentials.aws[:secret_access_key],
-      region:                'ap-northeast-1'
+      :provider              => 'AWS',
+      :region                => ENV['S3_REGION'],     
+      :aws_access_key_id     => ENV['S3_ACCESS_KEY'],
+      :aws_secret_access_key => ENV['S3_SECRET_KEY']
     }
-    config.fog_directory  = 'youchika-blog-bucket'
-    config.fog_public     = true
-    config.fog_attributes = { cache_control: "public, max-age=#{365.days.to_i}" }
-    end
+    config.fog_directory     =  ENV['S3_BUCKET']
   end
+end
