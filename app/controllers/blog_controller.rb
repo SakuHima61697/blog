36233c3a4148db5b3610bb9eee7b20f96d5ed52a
youchika41781 <kn3om21c8a28h8yi1ya0@gmail.com>
2020-11-12 07:40:08 +0900

    blog-deploy-after-version209 modify nilClass comment

--- app/controllers/blog_controller.rb
+++ app/controllers/blog_controller.rb
@@ -34,10 +34,10 @@ class BlogController < ApplicationController
 
   #ブログ詳細
   def show
-    @post = Post.find_by(id: params[:id])
-    @user = User.find_by(id: @post.user_id)
+    @post = Post&.find_by(id: params[:id])
+    @user = User&.find_by(id: @post.user_id)
     #コメント表示
-    @comments = Comment.all.order(id: "ASC").page(params[:page]).per(10)
+    @comments = Comment&.all.order(id: "ASC").page(params[:page]).per(10)
   end
   
   #ブログ詳細(コメント欄)
