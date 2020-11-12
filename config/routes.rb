2bb62b71efa14343e3567bed93a4746c79db7028
youchika41781 <kn3om21c8a28h8yi1ya0@gmail.com>
2020-11-13 00:26:30 +0900

    blog-deploy-after-version255 modify routes.rb line28

--- config/routes.rb
+++ config/routes.rb
@@ -25,7 +25,7 @@ Rails.application.routes.draw do
   post "blogs/show/:id" => "blog#newComment"
   
   #コメント削除
-  post "blogs/delete_comment" => "blog#deleteComment"
+  post "blogs/show/:id" => "blog#deleteComment"
 
   #ログインページ
   get 'blogs/login' => "user#login_form"
