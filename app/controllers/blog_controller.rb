175062292453e6d7371483a212d6e3480300c129
youchika41781 <kn3om21c8a28h8yi1ya0@gmail.com>
2020-11-13 01:07:00 +0900

    blog-deploy-after-version259

--- app/controllers/blog_controller.rb
+++ app/controllers/blog_controller.rb
@@ -59,7 +59,7 @@ class BlogController < ApplicationController
   def deleteComment
     @post = Post.find_by(id: params[:id])
     @comment = Comment.find_by(id: params[:id])
-    if @comment.destroy
+    if @comment&.destroy
       flash[:alert] = "コメントを削除しました！"
       redirect_to("/blogs/show/#{@post.id}")
     else
