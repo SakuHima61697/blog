9c3d538741478b0a89ceeb36e724a2ab156a096c
youchika41781 <kn3om21c8a28h8yi1ya0@gmail.com>
2020-11-15 10:24:15 +0900

    blog-deploy-after-version321 modify user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,7 +11,4 @@ class User < ApplicationRecord
     has_many :posts, foreign_key: "user_id"
     has_many :comments, foreign_key: "user_id"
     
-    default_value_for :name, "(no name)"
-    
-    
 end
