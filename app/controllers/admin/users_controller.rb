b1c3ff8184fde29694901b575c145512117bdcf8
youchika41781 <kn3om21c8a28h8yi1ya0@gmail.com>
2020-11-15 17:20:57 +0900

    blog-deploy-after-version327 modify admin login_form

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -11,7 +11,7 @@ before_action :forbid_admin_login_user, {only: [:login_form, :login]}
 
   #ログイン処理
   def login
-    @user = User.find_by(email: params[:email])
+    @user = User.find_by!(email: params[:email])
     if @user && @user&.authenticate(params[:password])
       session[:user_id] = @user.id
       session[:admin] = @user.admin
